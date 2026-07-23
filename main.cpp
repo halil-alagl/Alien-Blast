6dc34961878ddbce58b3ede2dde74193a32b1187
halil-alagl <halil0ibrahim0alagul@gmail.com>
2026-07-23 18:40:01 +0300

    Added movement controls

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 int main() {
     sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "Star Chaser");
     sf::RectangleShape player({ 30.f, 40.f });
-    player.setPosition({ 400.f, 300.f });
+    player.setPosition({ 400.f, 550.f });
 
     while (window.isOpen()) {
         while (const std::optional event = window.pollEvent()) {
@@ -11,10 +11,14 @@ int main() {
                 window.close();
         }
 
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left))
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left) || sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
             player.move({ -0.1f, 0.f });
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right))
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right) || sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
             player.move({ 0.1f, 0.f });
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up) || sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
+            player.move({ 0.f, -0.1f });
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down) || sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
+            player.move({ 0.f, 0.1f });
 
         window.clear();
         window.draw(player);
